708728da9718407dbf85aba5f30621fb4eabedb0
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2024-10-30 21:04:27 -0500

    chore(config): initial config

---
+++ vitest.config.ts
@@ -0,0 +1,25 @@
+// eslint-disable-next-line import/default
+import react from '@vitejs/plugin-react';
+import path from 'node:path';
+import { defineConfig } from 'vitest/config';
+
+// needed this way by vitest
+// eslint-disable-next-line import/no-default-export
+export default defineConfig({
+    plugins: [react()],
+    test: {
+        coverage: {
+            provider: 'v8',
+            reporter: ['text', 'html'],
+        },
+        css: true,
+        environment: 'jsdom',
+        globals: true,
+        setupFiles: './setup.react.ts',
+    },
+    resolve: {
+        alias: {
+            '~': path.resolve(__dirname, './src'),
+        },
+    },
+});
