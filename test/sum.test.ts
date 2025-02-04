c06ad6cc75e003b959fbb1562f8019bef88c667a
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2025-02-04 12:05:25 -0600

    chore(config): updates formatting

--- test/sum.test.ts
+++ test/sum.test.ts
@@ -1,5 +1,6 @@
-import { sum } from 'src';
 import { describe, expect, it } from 'vitest';
+
+import { sum } from '../src';
 
 describe('index function', () => {
   it('should add two numbers correctly', () => {
